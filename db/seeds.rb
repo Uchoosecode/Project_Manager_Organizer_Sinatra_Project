744ba59e62e2dfd0eeb0568f90c67212724601d5
Uchoosecode <awittyl44@gmail.com>
2020-02-15 06:35:05 -0500

    corrected the date input

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ Manager.create(:name => "Peter", :email => "peterproject@email.com", :password =
 Manager.create(:name => "Susan", :email => "susanproject@email.com", :password => "Susan")
 
 
-Project.create(:name => "Spring St", :project_type => "reconstruction", :start_date => "01/20/19", :due_date => Date.today.next, :manager_id => Manager.first.id )
-Project.create(:name => "MSG", :project_type => "transformation", :start_date => "08/15/2012", :due_date => "10/22/2019", :manager_id => Manager.second.id )
+Project.create(:name => "Spring St", :project_type => "reconstruction", :start_date => "Jan. 01, 2019", :due_date => Date.today.next, :manager_id => Manager.first.id )
+Project.create(:name => "MSG", :project_type => "transformation", :start_date => "Aug. 05, 2012", :due_date => "Oct. 22, 2019", :manager_id => Manager.second.id )
