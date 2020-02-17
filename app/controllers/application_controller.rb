41c4034fbe368aea41e14f02fb2d47f158737b0f
Uchoosecode <awittyl44@gmail.com>
2020-02-17 00:26:03 -0500

    missing } for interpolation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret "#{ENV['TOKEN']"
+    set :session_secret "#{ENV['TOKEN']}"
   end
 
   get "/" do
