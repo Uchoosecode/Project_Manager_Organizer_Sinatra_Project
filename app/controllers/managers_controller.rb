680a81ef0f76747658393dd34b9bd63874af1a87
Uchoosecode <awittyl44@gmail.com>
2020-02-20 09:16:49 -0500

    updated manager Controller

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,8 +1,27 @@
 class ManagersController < ApplicationController
 
-    get '/managers' do
-        @managers = Manager.all
-
-        erb :"/managers/index"
+    get '/signup' do
+        erb :"/manager/new"
     end
+    
+    post '/signup' do
+
+        manager = Author.new(params["manager"])
+    
+        if manager.save
+          session[:manager_id] = manager.id
+          redirect to "/manager"
+        
+        else
+          @errors = manager.errors.full_messages
+          erb :"/manager/new"
+        end
+      end
+
+      get '/manager/:id' do
+        # binding.pry
+        @manager = Manager.find_by_id(params[:id])
+        erb :"/manager/show"
+      end
+        
 end
