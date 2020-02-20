5ae8655228fc3012f9708b3259c50ff0a8f3d64f
Uchoosecode <awittyl44@gmail.com>
2020-02-20 09:21:35 -0500

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,28 @@
 class SessionsController < ApplicationController
   
+    # get '/sign_up' do
+    #     erb :"/managers/new"
+    #   end
+
     get '/login' do
         erb :"/sessions/new"
     end
 
 
     post '/login' do
-        
+        manager = Manager.find_by(email: params[:manager][:email])
+        # binding.pry
+        if manager && manager.authenticate(params[:manager][:password])
+            session[:manager_id] = manager.id
+            redirect to "/manager/#{manager.id}"
+
+        else
+            redirect to "/login"
+        end
+    end
+
+    post '/logout' do
+        session.clear
+        redirect to '/'
     end
 end
