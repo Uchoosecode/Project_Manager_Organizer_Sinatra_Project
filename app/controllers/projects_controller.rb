05d1afc264032f748513d26b3ffefaa88042ee9b
Uchoosecode <awittyl44@gmail.com>
2020-02-23 05:38:42 -0500

    added a not logged in method to controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,26 +1,30 @@
 class ProjectsController < ApplicationController
     
+    before  do
+        redirect_not_logged_in
+    end
+    
     get '/projects' do
         
-        @projects = Project.all
-        # binding.pry
-        # @projects.your_projects
+        @projects = current_manager.projects
         erb :"/projects/index"
     end
-
+    
     patch '/projects' do
         @projects = Project.find_by_id(params[:id])
-
+        
+        # binding.pry
         if @projects.update(params[:project])
-          redirect to "/projects/#{@projects.id}"
-        else
+          redirect to "/projects"
+        
+        else @errors = project.errors.full_messages
           erb :"/projects/edit"
         end
     end
 
-    # get '/projects/new' do
-    #     erb :"/projects/new"
-    #   end
+    get '/projects/new' do
+        erb :"/projects/new"
+      end
       
     post '/projects/new' do
         
@@ -29,43 +33,44 @@ class ProjectsController < ApplicationController
         if project.save
             redirect to "/projects"
         
-        else
-           @errors = project.errors.full_messages
+        else @errors = project.errors.full_messages
             erb :"/projects/new"
         end
     end
 
     get '/projects/:id' do
+        
         @projects = Project.find_by_id(params[:id])
         erb :"/projects/show"
     end
         
-    # get '/projects/:id/edit' do
-    #     @projects = Project.find_by_id(params[:id])
-    #     # binding.pry
-    #     erb :"projects/edit"
-    # end
-    
-    # patch '/projects/:id' do
-    #     @projects = Project.find_by_id(params[:id])
+    get '/projects/:id/edit' do
+        @projects = Project.find_by_id(params[:id])
         
-    #     if @projects.update(params[:projects])
-    #         redirect to "/projects/#{@projects.id}"
+        erb :"projects/edit"
+    end
+    
+    patch '/projects/:id' do
+        @projects = Project.find_by_id(params[:id])
+        if @projects.update(params[:projects])
             
-    #     else
-    #         erb :"projects/edit"
-    #     end
-    # end
+            redirect to "/projects"
+            
+        else @errors = project.errors.full_messages
+            erb :"projects/edit"
+        end
+    end
     
-    # delete '/projects/:id' do
-    #     @projects = Project.find_by_id(params[:id])
+    delete '/projects/:id' do
+        @projects = Project.find_by_id(params[:id])
 
-    #     if @projects
-    #         @projects.destroy
-    #     end
-    #     redirect to "/projects"
+        if @projects
+            @projects.destroy
+        end
         
-    # end
+        redirect to "/projects"
+        
+    end
         
 
 end
