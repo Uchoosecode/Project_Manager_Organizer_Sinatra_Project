12f09ae993608a0a36682079313fdae7039d1b46
Uchoosecode <awittyl44@gmail.com>
2020-02-26 14:40:34 -0500

    removed binding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   
-
     get '/login' do
         erb :"/sessions/new"
     end
 
     post '/login' do
         manager = Manager.find_by(email: params[:manager][:email])
-        # binding.pry
+        
         if manager && manager.authenticate(params[:manager][:password])
+
             session[:manager_id] = manager.id
             redirect to "/manager/#{manager.id}"
 
